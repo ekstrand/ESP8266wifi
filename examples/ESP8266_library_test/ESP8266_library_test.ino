a9caa113c25dc3978e6ade1e38256f40dcd5ed25
Jonas Ekstrand <jonas@inspirativ.se>
2015-03-15 13:50:49 +0100

    Adding some comments and making the example easier to understand.

--- examples/ESP8266_library_test/ESP8266_library_test.ino
+++ examples/ESP8266_library_test/ESP8266_library_test.ino
@@ -1,11 +1,14 @@
 #include <SoftwareSerial.h>
 #include <ESP8266wifi.h>
 
-SoftwareSerial swSerial(8, 9, false);
+#define sw_serial_rx_pin 4
+#define sw_serial_rx_pin 6
+#define esp8266_reset_pin 5 // Connect this pin to CH_PD on the esp8266, not reset. (let reset be unconnected)
 
-// 10 is the pin used to reset the esp8266 
+SoftwareSerial swSerial(sw_serial_rx_pin, sw_serial_rx_pin);
+
 // the last parameter sets the local echo option for the ESP8266 module..
-ESP8266wifi wifi(swSerial, swSerial, 10, true); 
+ESP8266wifi wifi(swSerial, swSerial, esp8266_reset_pin, true);
 
 String inputString;
 boolean stringComplete = false;
@@ -13,45 +16,39 @@ unsigned long nextPing = 0;
 
 void setup() {
   inputString.reserve(20);
-  pinMode(6, INPUT); //RX
-  pinMode(7, OUTPUT); //TX
-
   swSerial.begin(9600);
-
   Serial.begin(9600);
   while (!Serial)
     ;
-    
+  Serial.println("Starting wifi");
+
   //Set config options
   wifi.debug(Serial); //Will print everything from esp8266 to Serial stream of you choice
+
+  wifi.setTransportToTCP();// this is also default
   // wifi.setTransportToUDP();//Will use UDP when connecting to server, default is TCP
-  // wifi.setTransportToTCP();//Will change back to using TCP
+
   wifi.endSendWithNewline(true); // Will end all transmissions with a newline and carrage return ie println.. default is true
-  
+
   wifi.begin();
-  wifi.connectToAP("AP_SSID", "AP_PASSWORD");
-  wifi.connectToServer("192.168.5.123", "2121");
+  wifi.connectToAP("ComHem719F6D", "kufp7dy7");
+  wifi.connectToServer("192.168.0.28", "2121");
   wifi.send(SERVER, "ESP8266 test app started");
 }
 
 void loop() {
+  
+  //Make sure the esp8266 is started..
   if (!wifi.isStarted())
-      wifi.begin();
-  /*
-  //Turn on local ap and server if we are not connected, otherwise turn off (at this time, local server is TCP only)
-   if(!wifi.isConnectedToServer() && !wifi.isLocalAPAndServerRunning())
-   wifi.startLocalAPAndServer("MY_CONFIG_AP", "password", "5", "2121");
-   else if(wifi.isConnectedToServer() && wifi.isLocalAPAndServerRunning())
-   wifi.stopLocalAPAndServer();
-   */
-
+    wifi.begin();
+    
+  //Send what you typed in the arduino console to the server
   static char buf[20];
   if (stringComplete) {
     inputString.toCharArray(buf, sizeof buf);
     wifi.send(SERVER, buf);
     inputString = "";
     stringComplete = false;
-    nextPing = millis() + 10000;
   }
 
   //Send a ping once in a while..
@@ -60,7 +57,7 @@ void loop() {
     nextPing = millis() + 10000;
   }
 
-  //Listen for responses
+  //Listen for incoming messages and echo back, will wait until a message is received, or max 6000ms..
   WifiMessage in = wifi.listenForIncomingMessage(6000);
   if (in.hasData) {
     Serial.println(in.message);
@@ -71,6 +68,7 @@ void loop() {
   }
 }
 
+//Listen for serial input from the console
 void serialEvent() {
   while (Serial.available()) {
     char inChar = (char)Serial.read();
