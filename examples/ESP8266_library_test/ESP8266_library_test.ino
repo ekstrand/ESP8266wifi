a6f4d22328352d07377364611711a71ee6f15561
Jonas Ekstrand <jonas@inspirativ.se>
2015-04-07 20:00:08 +0200

    Fixing typo in example code and adding comment to which pin rx and tx connects to.

--- examples/ESP8266_library_test/ESP8266_library_test.ino
+++ examples/ESP8266_library_test/ESP8266_library_test.ino
@@ -1,11 +1,11 @@
 #include <SoftwareSerial.h>
 #include <ESP8266wifi.h>
 
-#define sw_serial_rx_pin 4
-#define sw_serial_rx_pin 6
+#define sw_serial_rx_pin 4 //  Connect this pin to TX on the esp8266
+#define sw_serial_tx_pin 6 //  Connect this pin to TX on the esp8266
 #define esp8266_reset_pin 5 // Connect this pin to CH_PD on the esp8266, not reset. (let reset be unconnected)
 
-SoftwareSerial swSerial(sw_serial_rx_pin, sw_serial_rx_pin);
+SoftwareSerial swSerial(sw_serial_rx_pin, sw_serial_tx_pin);
 
 // the last parameter sets the local echo option for the ESP8266 module..
 ESP8266wifi wifi(swSerial, swSerial, esp8266_reset_pin, Serial);//adding Serial enabled local echo and wifi debug
