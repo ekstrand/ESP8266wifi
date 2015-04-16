65b74b7075193ef36ae0fed592c490052b997e38
Jonas Ekstrand <jonas@inspirativ.se>
2015-04-16 16:33:12 +0200

    Update ESP8266_library_test.ino

--- examples/ESP8266_library_test/ESP8266_library_test.ino
+++ examples/ESP8266_library_test/ESP8266_library_test.ino
@@ -2,7 +2,7 @@
 #include <ESP8266wifi.h>
 
 #define sw_serial_rx_pin 4 //  Connect this pin to TX on the esp8266
-#define sw_serial_tx_pin 6 //  Connect this pin to TX on the esp8266
+#define sw_serial_tx_pin 6 //  Connect this pin to RX on the esp8266
 #define esp8266_reset_pin 5 // Connect this pin to CH_PD on the esp8266, not reset. (let reset be unconnected)
 
 SoftwareSerial swSerial(sw_serial_rx_pin, sw_serial_tx_pin);
